d002d41f0c323df45127a17c6d390dbd0326571d
Ri7Sh <sonlshrm67@yahoo.com>
2018-02-20 02:08:49 +0530

    database changed

--- checkmate_apogee/settings.py
+++ checkmate_apogee/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'checkmate_apogee.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'checkmate',
+        'USER': 'checkmate',
+        'PASSWORD': 'phodenge',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
